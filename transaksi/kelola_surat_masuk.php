82f2f9070ec939d89ecccf81b6f58378ff360c03
eofficekesdamjaya-hawr <eofficekesdamjaya@gmail.com>
2026-06-26 01:40:57 +0700

    update kelola_surat_masuk.php

--- transaksi/kelola_surat_masuk.php
+++ transaksi/kelola_surat_masuk.php
@@ -366,6 +366,7 @@ function displayTableSuratMasuk($result, $user_email, $allowed_emails, $ruanganM
 <?php 
 }
 ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
